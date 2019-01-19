a0ea28d9980f4d40bf3bdbc654fb6731ac743bd9
Timothy Dick <timothydicknz@gmail.com>
2019-01-19 20:54:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ module.exports = { name, height
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
-    console.log("Expected the name to be Susan")
+    console.log("Steve and Susan")
 }
